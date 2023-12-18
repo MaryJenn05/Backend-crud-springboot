34b302e86a23ba7ed3a30fe070e151ec51ec02a3
Jenn Espinoza <jenniespinoza2002@hotmail.com>
2023-12-18 00:13:45 -0500

    initial spring boot setup

---
+++ entity-demo/src/main/java/com/crud/entitydemo/EntityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.crud.entitydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityDemoApplication.class, args);
+	}
+
+}
---
+++ entity-demo/src/test/java/com/crud/entitydemo/EntityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.entitydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
